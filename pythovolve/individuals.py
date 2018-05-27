6ee2c845ea2f3a506875c1ff9103addbd46bb92a
Peter Schmidbauer <peter.schmidb@gmail.com>
2018-05-27 15:26:40 +0200

    remove abc from individual

--- pythovolve/individuals.py
+++ pythovolve/individuals.py
@@ -1,29 +1,29 @@
 import random
-from abc import abstractmethod, ABCMeta
-from typing import Any, List
+from typing import Any, List, Callable
 
 
-class Individual(metaclass=ABCMeta):
+class Individual:
+    def __init__(self, phenotype: Any):
+        self._score: float = None
+        self.phenotype = phenotype
+
     @property
-    @abstractmethod
-    def phenotype(self) -> Any:
-        pass
+    def score(self) -> float:
+        if not self._score:
+            raise ValueError("Individual has not been scored yet")
+        return self._score
 
-    @phenotype.setter
-    @abstractmethod
-    def phenotype(self, phenotype: Any) -> None:
-        pass
+    def calculate_score(self, score_func: Callable):
+        self._score = score_func(self)
 
     def __repr__(self):
         return f"{type(self).__name__}({self.phenotype})"
 
 
 class BinaryIndividual(Individual):
-    phenotype: List[bool] = None
 
-    def __init__(self, binary: List[bool]):
-        super().__init__()
-        self.phenotype = binary
+    def __init__(self, phenotype: List[bool]):
+        super().__init__(phenotype)
 
     @classmethod
     def create_random(cls, num_bits: int):
@@ -36,9 +36,8 @@ class BinaryIndividual(Individual):
 class PathIndividual(Individual):
     phenotype: List[int] = None
 
-    def __init__(self, path: List[int]):
-        super().__init__()
-        self.phenotype = path
+    def __init__(self, phenotype: List[int]):
+        super().__init__(phenotype)
 
     @classmethod
     def create_random(cls, num_cities: int):
@@ -46,9 +45,3 @@ class PathIndividual(Individual):
 
     def __str__(self):
         return "[" + " -> ".join((str(city) for city in self.phenotype)) + "]"
-
-
-class Population:
-    pass
-
-
