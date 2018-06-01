652c303f415f1132c22191f0b5d8ff3dd37eac97
Peter Schmidbauer <peter.schmidb@gmail.com>
2018-06-01 18:36:08 +0200

    add simple setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pythovolve',
+      version='0.1',
+      description='Object oriented framework for genetic algorithms',
+      url='https://github.com/peter-schmidbauer/pythovolve',
+      author='Peter Schmidbauer',
+      author_email='peter.schmidb@gmail.com',
+      license='MIT',
+      packages=['pythovolve'],
+      install_requires=["matplotlib"],
+      zip_safe=False)
