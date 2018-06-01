84752071dd39e5349e306f872741afed3130409a
Peter Schmidbauer <peter.schmidb@gmail.com>
2018-06-01 20:58:40 +0200

    add more tournament and linearrank selectors

--- pythovolve/selection.py
+++ pythovolve/selection.py
@@ -18,3 +18,25 @@ class ProportionalSelector(Selector):
     def __call__(self, population: List[Individual]) -> Individual:
         weights = [indiv.score for indiv in population]
         return random.choices(population, weights, k=1)[0]
+
+
+class LinearRankSelector(Selector):
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, population: List[Individual]) -> Individual:
+        population = sorted(population, key=lambda ind: ind.score)
+        ranks = range(1, len(population) + 1)
+        sum_ranks = sum(ranks)
+        weights = [i / sum_ranks for i in ranks]
+        return random.choices(population, weights, k=1)[0]
+
+
+class TournamentSelector(Selector):
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, population: List[Individual], tournament_size=5) -> Individual:
+        chosen = random.choices(population, k=tournament_size)
+        return max(chosen, key=lambda ind: ind.score)
+
