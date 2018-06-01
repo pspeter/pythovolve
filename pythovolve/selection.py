bce290a6d3303871aab2e9c031358e6b900b2afe
Peter Schmidbauer <peter.schmidb@gmail.com>
2018-06-01 23:15:41 +0200

    add multiselection

--- pythovolve/selection.py
+++ pythovolve/selection.py
@@ -1,13 +1,13 @@
 import random
 from abc import ABCMeta, abstractmethod
-from typing import List
+from typing import Sequence
 
 from pythovolve.individuals import Individual
 
 
 class Selector(metaclass=ABCMeta):
     @abstractmethod
-    def __call__(self, population: List[Individual]) -> Individual:
+    def __call__(self, population: Sequence[Individual]) -> Individual:
         pass
 
 
@@ -15,7 +15,7 @@ class ProportionalSelector(Selector):
     def __init__(self):
         super().__init__()
 
-    def __call__(self, population: List[Individual]) -> Individual:
+    def __call__(self, population: Sequence[Individual]) -> Individual:
         weights = [indiv.score for indiv in population]
         return random.choices(population, weights, k=1)[0]
 
@@ -24,7 +24,7 @@ class LinearRankSelector(Selector):
     def __init__(self):
         super().__init__()
 
-    def __call__(self, population: List[Individual]) -> Individual:
+    def __call__(self, population: Sequence[Individual]) -> Individual:
         population = sorted(population, key=lambda ind: ind.score)
         ranks = range(1, len(population) + 1)
         sum_ranks = sum(ranks)
@@ -36,7 +36,23 @@ class TournamentSelector(Selector):
     def __init__(self):
         super().__init__()
 
-    def __call__(self, population: List[Individual], tournament_size=5) -> Individual:
+    def __call__(self, population: Sequence[Individual], tournament_size=5) -> Individual:
         chosen = random.choices(population, k=tournament_size)
         return max(chosen, key=lambda ind: ind.score)
 
+
+class MultiSelector(Selector):
+    def __init__(self, selectors: Sequence[Selector], weights: Sequence[float] = None):
+        super().__init__()
+        self.selectors = selectors
+        self.weights = weights
+
+    def __call__(self, population: Sequence[Individual]) -> Individual:
+        selector = random.choices(self.selectors, self.weights, k=1)[0]
+        return selector(population)
+
+
+proportional_selector = ProportionalSelector()
+linear_rank_selector = LinearRankSelector()
+tournament_selector = TournamentSelector()
+multi_selector = MultiSelector((proportional_selector, linear_rank_selector, tournament_selector))
