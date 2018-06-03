82a37ac8022cd83a6ca9a4ef20b6cdc31bcf4e38
Peter Schmidbauer <peter.schmidb@gmail.com>
2018-06-03 23:58:51 +0200

    replace all windows CRLF line endings with linux LF

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup
 
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
 setup(name='pythovolve',
       version='0.1',
       description='Object oriented framework for genetic algorithms',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/peter-schmidbauer/pythovolve',
       author='Peter Schmidbauer',
       author_email='peter.schmidb@gmail.com',
       license='MIT',
       packages=['pythovolve'],
       install_requires=["matplotlib", "seaborn", 'sympy'],
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=(
+          "Programming Language :: Python :: 3.6",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Topic :: Scientific/Engineering :: Artificial Intelligence"
+      ))
